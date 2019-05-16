49be99b438c39cf8ddbb14b43d2471c782bd3fdf
Hans Bélanger <hans.darmstadt-belanger1@uqac.ca>
2019-05-16 14:09:11 -0400

    started working on property3.

---
+++ Source/Core/src/Main.java
@@ -0,0 +1,32 @@
+import ca.uqac.lif.cep.Connector;
+import ca.uqac.lif.cep.io.Print;
+import ca.uqac.lif.cep.tmf.Pump;
+import hipDistance.HipDistance;
+import utilityFeatures.ParseFileToJson;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ParseFileToJson myParser = new ParseFileToJson("long_trace.json");
+
+        HipDistance prop = new HipDistance();
+        /*Connector.connect(myParser, prop);
+
+        Pump myPump = new Pump(0);
+        Connector.connect( prop, myPump);
+
+        Print p = new Print().setSeparator("\n\n");
+        Connector.connect(myPump, p);
+        myPump.run();*/
+
+        Pump testPump = new Pump(0);
+        Connector.connect(myParser, testPump);
+        Print p = new Print().setSeparator("\n\n");
+        Connector.connect(testPump, p);
+        testPump.run();
+
+
+    }
+}
+
---
+++ Source/Core/src/hipDistance/HipDistance.java
@@ -0,0 +1,19 @@
+package hipDistance;
+
+import ca.uqac.lif.cep.GroupProcessor;
+import ca.uqac.lif.cep.functions.ApplyFunction;
+import utilityFeatures.GetJsonFields;
+
+public class HipDistance extends GroupProcessor {
+
+    private ApplyFunction getId;
+
+    public HipDistance() {
+        super(1,1);
+
+      //  getId = new ApplyFunction( new GetJsonFields(GetJsonFields.))
+
+
+
+    }
+}
