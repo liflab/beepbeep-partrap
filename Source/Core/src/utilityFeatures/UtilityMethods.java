51d9ee8ff033108cb8139910314db329790056ee
HBarbosa <hbarbosapro@gmail.com>
2019-05-17 16:16:55 +0200

    Add UtilityMethods class that allow to create redundant method.
    Here we just add a method that allow to calculate distance between two
    points in 3D.

---
+++ Source/Core/src/utilityFeatures/UtilityMethods.java
@@ -0,0 +1,23 @@
+package utilityFeatures;
+
+/**
+ * @author Helloïs BARBOSA
+ *
+ */
+public class UtilityMethods {
+	
+	/**
+	 * This method allow to calculate the distance between two points in 3D and return it as a float
+	 * @param firstPoint
+	 * @param secondPoint
+	 * 						firstPoint & secondPoint are array of Float with in index 0: x position, in index 1: y position and in index 2: z position
+	 * 
+	 */
+	public static Float distanceProcessing(Float[] firstPoint, Float[] secondPoint) {
+		return (float) Math.sqrt(
+				Math.pow(firstPoint[0].doubleValue() - secondPoint[0].doubleValue(), 2) + 
+				Math.pow(firstPoint[1].doubleValue() - secondPoint[1].doubleValue(), 2) +
+				Math.pow(firstPoint[2].doubleValue() - secondPoint[2].doubleValue(), 2));
+	}
+
+}
